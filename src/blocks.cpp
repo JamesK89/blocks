bd742b6703e066effcefa1105064f8ebd0bc9f94
James J. Kelly Jr <jamesk89@gmail.com>
2019-02-15 14:07:29 -0500

    Updated Visual Studio project. Fixed problems with SetGameState not returning a value.

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -566,6 +566,8 @@ BaseGameState* Application::GetGameState(void)
 
 BaseGameState* Application::SetGameState(BaseGameState* state)
 {
+	BaseGameState* result = nullptr;
+
 	if (currentGameState_ != state)
 	{
 		if (currentGameState_)
@@ -575,17 +577,24 @@ BaseGameState* Application::SetGameState(BaseGameState* state)
 		
 		state->OnResume(currentGameState_);
 		currentGameState_ = state;
+
+		result = currentGameState_;
 	}
+
+	return result;
 }
 
 BaseGameState* Application::SetGameState(const char* szStateName)
 {
+	BaseGameState* result = nullptr;
 	BaseGameState* newState = GetGameState(szStateName);
 	
 	if (newState)
 	{
-		SetGameState(newState);
+		result = SetGameState(newState);
 	}
+
+	return result;
 }
 
 int main(int argc, char* argv[])
